5163fe352a1b37dd6de79d29dc32e8c2d34d1a16
napittipong <76505529+napittipong@users.noreply.github.com>
2021-02-17 12:22:23 +0700

    Add files via upload

---
+++ Appheader.js
@@ -0,0 +1,12 @@
+function Appheader() {
+
+return (
+    <header className="App">
+      <img className="App-header" src = "/pic/favicon.ico"/>
+        
+
+
+      </header>
+        );
+    }
+  export default Appheader;
---
+++ items.js
@@ -0,0 +1,23 @@
+const items =[
+    {
+      title :"น้องแมว",
+      thumbnaiUrl :"/pic/123.jpg"
+    },
+
+    {
+      title :"น้อนแมว",
+      thumbnaiUrl :"/pic/1234.jpg"
+    },
+
+    {
+      title :"เจ้าแมว",
+      thumbnaiUrl :"/pic/12345.jpg"
+    },
+
+    {
+      title :"ไอ้แมว",
+      thumbnaiUrl :"/pic/123456.jpg"
+    }
+    
+  ];
+  export default items;
